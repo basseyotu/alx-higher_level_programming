c261a418e12f9b9695cb990b4d6c9066f423674c
basseyotu <118821086+basseyotu@users.noreply.github.com>
2023-03-07 15:13:34 +0100

    continued

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,56 +1,43 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
-
 /**
- * insert_node - insert node in a linked list
+ * insert_node - add node to list
+ * @head: head
+ * @number: node
  *
- * @head: pointer to the first node of the linked list
+ * Return: new node
  *
- * @number: integer value of the new node to insert
- *
- * Return: the head of the linked list with the new node inserted
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *new_head = malloc(sizeof(listint_t));
-	listint_t *tmp = *head;
+	listint_t *node, *aux = *head, *aux1;
 
-	if (new_head == NULL)
+	if (!head)
 	{
 		return (NULL);
 	}
-	if (tmp == NULL)
+
+	node = malloc(sizeof(listint_t));
+	if (!node)
 	{
-		new_head->n = number;
-		new_head->next = NULL;
-		(*head) = new_head;
-		return (new_head);
+		return (NULL);
 	}
-	if (tmp->next == NULL || number == 0)
+
+	node->n = number;
+	if (!aux || aux->n >= number)
 	{
-		if (number < tmp->n)
-		{
-			new_head->n = number;
-			new_head->next = tmp;
-			(*head) = new_head;
-			return (new_head);
-		}
+		node->next = aux, *head = node;
+		return (node);
 	}
-	while (tmp->next)
+
+	aux1 = aux->next;
+	while (aux && aux1 && (aux1->n < number))
 	{
-		if ((number >= tmp->n) && (number <= tmp->next->n))
-		{
-			new_head->n = number;
-			new_head->next = tmp->next;
-			tmp->next = new_head;
-			return (new_head);
-		}
-		tmp = tmp->next;
+		aux = aux->next, aux1 = aux->next;
 	}
-	new_head->n = number;
-	new_head->next = NULL;
-	tmp->next = new_head;
-	return (new_head);
+
+	aux->next = node, node->next = aux1;
+
+	return (node);
 }
